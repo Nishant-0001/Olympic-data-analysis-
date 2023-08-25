845365a5949293ca2f542845d0b627b71fff4cc8
chicku31 <88822119+chicku31@users.noreply.github.com>
2023-08-25 22:47:21 +0530

    Update DataBricks.py

--- code/DataBricks.py
+++ code/DataBricks.py
@@ -1,17 +1,20 @@
 from pyspark.sql.functions import col
 from pyspark.sql.types import IntegerType, DoubleType, BooleanType, DateType
 from pyspark.sql.functions import avg
+
 configs = {"fs.azure.account.auth.type": "OAuth",
 "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-"fs.azure.account.oauth2.client.id": "1a341cbe-d54f-4e20-adb0-62b957ef8947",
-"fs.azure.account.oauth2.client.secret":"unU8Q~ZXWalRSGWz~bJ2cZKEieRHIdqPjV2BSabg",
-"fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/4afcaf18-e417-4791-9b62-457b3caf4863/oauth2/token"}
+"fs.azure.account.oauth2.client.id": "",
+"fs.azure.account.oauth2.client.secret": '',
+"fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/tanent_id/oauth2/token"}
 
 
 dbutils.fs.mount(
-    source="abfss://tokyo-olympic-data@tokyoolympics31.dfs.core.windows.net", # container@storageacc
-    mount_point="/mnt/tokyoolymic",
- extra_configs=configs)
+source = "abfss://tokyo-olympic-data@tokyoolympicdata.dfs.core.windows.net", # contrainer@storageacc
+mount_point = "/mnt/tokyoolymic",
+extra_configs = configs)
+
+
 %fs
 ls "/mnt/tokyoolymic"
 Athletes=spark.read.format("csv").option("header","True").option("inferSchema","True").load("/mnt/tokyoolymic/raw-data/athletes.csv")
